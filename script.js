a2958eea551725f4f207b27b3b12aefda534f739
Noah Burton <nburton115@gmail.com>
2023-06-23 16:47:10 -0600

    "pseudo code and start for generatePassword funct"

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 // Assignment code here
+function generatePassword() {
+  // Get Password Length (8-128) to start
+  var passwordLength = prompt('Enter how many characters long you would like your new password to be (8 to 128)');
+  console.log('This password will be ' + parseInt(passwordLength) + ' characters in length');
+  passwordLength = parseInt(passwordLength);
+  // Validate User input in a number between 8 and 128
+
+  // Get Character Set
+    // Lowercase?
+    // Uppercase?
+    // Numeric?
+    // Special chars?
+  
+  //Validate Charset: At least one char set selected?  
+}
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
