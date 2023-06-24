6d61b742df7e936d9edcb4af980940f4c4e4c0e7
Noah Burton <nburton115@gmail.com>
2023-06-24 14:50:44 -0600

    fixed password lengeth while loop

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 // Assignment code here
 function generatePassword() {
   // Get Password Length (8-128) from user
-  var passwordLength = prompt('Enter how many characters long you would like your new password to be (8 to 128)');
-  console.log('This password will be ' + parseInt(passwordLength) + ' characters in length');
+  var passwordLength = prompt('Enter how many characters long you would like your new password to be (8 to 128)'); // sets password length to user input
   passwordLength = parseInt(passwordLength); // redefines the var initialized on line 4 to ONLY the number value of user input
-  // Validate User input is a number between 8 and 128 BROKEN!!
+  // Validate User input is a number between 8 and 128
   while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) { // if user input is : not a number, less than 8 chars, or more than 128...  
-    prompt('The length you entered is invalid. Enter a new length between 8 and 128.');
-    passwordLength = parseInt(passwordLength);
-    // TO DO: make while loop stop when a valid number is entered
+    passwordLength = parseInt(prompt('The length you entered is invalid. Enter a new length between 8 and 128.')); // they get this prompt
     if (!isNaN(passwordLength) && passwordLength >= 8 && passwordLength <= 128) {
-      break;
+      break; // all these must be true to move on
     }
 
 
   }
-  confirm('Confirm the characters you would like to use for your password. For the following options, select ok to include that character set and cancel to exclude the character set.')
+  confirm('Confirm the characters you would like to use for your password. For the following options, select OK to include that character set and CANCEL to exclude the character set.')
   // Get Character Set
   var selectedChar = ''; 
 
