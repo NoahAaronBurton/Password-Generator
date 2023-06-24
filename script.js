b47262eb906b59e3c37f14d21b73215cafafc39e
Noah Burton <nburton115@gmail.com>
2023-06-24 15:59:06 -0600

    char select loop and choice validator

--- script.js
+++ script.js
@@ -5,24 +5,38 @@ function generatePassword() {
   passwordLength = parseInt(passwordLength); // redefines the var initialized on line 4 to ONLY the number value of user input
   // Validate User input is a number between 8 and 128
   while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) { // if user input is : not a number, less than 8 chars, or more than 128...  
-    passwordLength = parseInt(prompt('The length you entered is invalid. Enter a new length between 8 and 128.')); // they get this prompt
+    passwordLength = parseInt(prompt('The length you entered is invalid. Enter a new length between 8 and 128.')); // ...they get this prompt
     if (!isNaN(passwordLength) && passwordLength >= 8 && passwordLength <= 128) {
       break; // all these must be true to move on
     }
 
-
   }
-  confirm('Confirm the characters you would like to use for your password. For the following options, select OK to include that character set and CANCEL to exclude the character set.')
-  // Get Character Set
-  var selectedChar = ''; 
+  alert('Confirm the characters you would like to use for your password. For the following options, select OK to include that character set and CANCEL to exclude the character set.')
+  // Get Character Set from user
+  var selectedChars = ''; // init empty variable for char set questions
 
-    // Lowercase?
-    var includeLower = confirm ('Include lower case?') // converts user input to yes or no
-    // Uppercase?
-    // Numeric?
-    // Special chars?
   
-  //Validate Charset: At least one char set selected?  
+    // Lowercase?
+    var includeLower = confirm ('Include lower case?'); // declares new variable that is equal to the choice of user input (true or false)
+    // Uppercase?
+    var includeUpper = confirm ('Include upper case?');
+    // Numeric?
+    var includeNumeric = confirm('Include numbers?');
+    // Special chars?
+    var includeSpecial = confirm ('Include special characters? (%$#@...)');
+
+     //Validate Charset: At least one char set selected?  
+     while (includeLower === false && includeUpper === false && includeNumeric === false && includeSpecial === false) { // if cancel is selected for all the choices...
+    alert('You need to include at least one charset. Please try again.');
+    // Lowercase?
+    includeLower = confirm ('Include lower case?'); // redeclares the variables that is equal to the choice of user input (true or false)
+    // Uppercase?
+    includeUpper = confirm ('Include upper case?');
+    // Numeric?
+    includeNumeric = confirm('Include numbers?');
+    // Special chars?
+    includeSpecial = confirm ('Include special characters? (%$#@...)');
+  }
 
   // Generate password now that length and charset is established 
   var password = '';
